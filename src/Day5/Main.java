c3e31208f7f2e8f8b55e5ffca5c5718e63d0c69b
TheLlama1 <martingugov20a@gmail.com>
2023-07-09 12:38:41 +0300

    Homework5

--- src/Day5/Main.java
+++ src/Day5/Main.java
@@ -1,10 +1,22 @@
 package Day5;
 
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        Teacher teacher = new Teacher();
-        Student student = new Student();
-        teacher.TeacherInfo();
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        String gender = scanner.next();
+        int id = scanner.nextInt();
+        int clas = scanner.nextInt();
+        int numInClass = scanner.nextInt();
+        double grade = scanner.nextDouble();
+
+        //Teacher teacher = new Teacher();
+        Student student = new Student(name, gender, id, clas, numInClass, grade);
+        //teacher.TeacherInfo();
         student.StudentInfo();
     }
 }
--- src/Day5/Student.java
+++ src/Day5/Student.java
@@ -11,6 +11,52 @@ public class Student {
     int numberInClass;
     double grade;
 
+
+    public String getName(){
+        return this.name;
+    }
+    public String setName(String name){
+        return name;
+    }
+    public String getGender(String gender){
+        return gender;
+    }
+    public String setGender(String gender){
+        if(gender.equals("Male") || gender.equals("male") || gender.equals("Female") || gender.equals("female")){
+            return gender;
+        }else{
+            throw new IllegalArgumentException("Wrong value!");
+        }
+    }
+    public int getID(int id){
+        return id;
+    }
+    public int setID(int id){
+        return id;
+    }
+    public int getKlas(int clas){
+        if(clas > 12 || clas < 1){
+            throw new IllegalArgumentException("Wrong value!");
+        }
+            return clas;
+    }
+    public int getNumberInClass(int numInClass){
+        return numInClass;
+    }
+    public int setNumInClass(int numInClass){
+        if(numInClass < 1){
+            throw new IllegalArgumentException("Wrong value!");
+        }
+        return numInClass;
+    }
+    public Student(String name, String gender, int id, int clas, int numInClass, double grade){
+        this.name = name;
+        this.gender = gender;
+        this.ID = id;
+        this.klas = clas;
+        this.numberInClass = numInClass;
+
+    }
     public void StudentInfo(){
         System.out.println("Name: " + this.name);
         System.out.println("Gender: " + this.gender);
